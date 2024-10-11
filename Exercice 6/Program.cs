81f9ec0e338308f3ef1b8411ff16632df67b34ec
mosta <mosta@MOSTAGHFERA>
2024-10-11 10:21:36 +0200

    Ajoutez des fichiers projet.

---
+++ Exercice 6/Program.cs
@@ -0,0 +1,56 @@
+﻿/**
+ * * Exercice 6
+ * * Auteur: Mostaghfera Ahadi
+ * * date: 01/10/2024
+ */
+using System;
+
+
+namespace Exercice_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float moyenne;
+
+            Console.Write("Entrez votre moyenne = ");
+            moyenne = float.Parse(Console.ReadLine());
+            if (moyenne>=16)
+            {
+                Console.Write("très bien");
+            }
+            else
+            {
+                if (moyenne>=14)
+                {
+                    Console.Write("bien");
+                }
+                else
+                {
+                    if (moyenne>=12)
+                    {
+                        Console.Write("assez bien");
+                    }
+                    else
+                    {
+                        if (moyenne >=10)
+                        {
+                            Console.Write("passable");
+                        }
+                        else
+                        {
+                            if (moyenne < 10)
+                            {
+                                Console.Write("recalé");
+                            }
+                        }
+
+                    }
+                }
+               
+            }
+            Console.ReadLine();
+        }
+    }
+}
